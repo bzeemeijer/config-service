40de17c3ad5aa0f23ad571741375013d9c3f1ba9
bjornz <bjorn.zeemeijer@pharmapartners.nl>
2018-08-15 14:41:29 +0200

    Minor refactoring and code cleanup

--- src/main/java/nl/zeemeijer/tools/config/Application.java
+++ src/main/java/nl/zeemeijer/tools/config/Application.java
@@ -4,10 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Spring Boot application which implements a Spring Cloud Config Server
+ *
+ * @author Bjorn Zeemeijer
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
 
+    /**
+     * Main entrypoint of the application.
+     *
+     * @param args arguments passed to the application
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
